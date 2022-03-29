5d59c5b32d9eaf73986f60f29e0c13bff5a02bc9
Rosario Garufi <saro.garufi1@gmail.com>
2022-03-29 16:27:01 +0200

    Javascript problema ciclo for e condizione if

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,32 @@ document.querySelector(".btn").onclick = function(){
     let nome = document.getElementById("nome_utente").value;
     console.log(nome);
     document.getElementById("utente").innerHTML=(nome);
-    let container_mail=["saro.garufi1@gmail.com", "pippo@gmail.com", "paperino@hotmail.it", "giovannisunta@live.it"];
     let mail = document.getElementById("mail").value;
+
 console.log(mail);
-if(container_mail == mail){
-    alert("ok");
-}else{
-    alert("non puoi accedere");
+
+
+let mail_autorizzate=["saro.garufi1@gmail.com", "pippo@gmail.com", "paperino@hotmail.it", "giovannisunta@live.it"];
+for(let i = 0; i<mail_autorizzate.length; i++){
+    console.log(mail_autorizzate[i]);
+    if(mail == mail_autorizzate[i]){
+        console.log("Accesso approvato");
+    }else{
+        console.log("Accesso negato");
+    }
 }
+
+    
+    
+
 }
 
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-//1) chiedere all'utente l'email
 
 
+
+   
+
+
+
+    
