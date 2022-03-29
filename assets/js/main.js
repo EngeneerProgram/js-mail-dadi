35847a95ca0c45972283d33c139f50a2569f79c7
Rosario Garufi <saro.garufi1@gmail.com>
2022-03-29 15:42:12 +0200

    changers

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,14 @@ document.querySelector(".btn").onclick = function(){
     let nome = document.getElementById("nome_utente").value;
     console.log(nome);
     document.getElementById("utente").innerHTML=(nome);
-    
+    let container_mail=["saro.garufi1@gmail.com", "pippo@gmail.com", "paperino@hotmail.it", "giovannisunta@live.it"];
     let mail = document.getElementById("mail").value;
 console.log(mail);
+if(container_mail == mail){
+    alert("ok");
+}else{
+    alert("non puoi accedere");
+}
 }
 
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
